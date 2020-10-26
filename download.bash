57aa16d282812d85971c2afaa6a875c8b7e0df05
Justas Mundeikis <mundeikis@gmx.de>
2020-10-26 23:09:03 +0200

    update

---
+++ download.bash
@@ -0,0 +1,16 @@
+!/usr/bin/bash
+
+wget -O /home/pc/Data/Projects/Atvira_sodra_duomenys/vidurkiai_$(date +%Y_%m_%d).zip -c http://sodra.is.lt/Failai/Vidurkiai.zip;
+wget -O /home/pc/Data/Projects/Atvira_sodra_duomenys/skolos_$(date +%Y_%m_%d).zip -c http://sodra.is.lt/Failai/Skolos.zip;
+wget -O /home/pc/Data/Projects/Atvira_sodra_duomenys/apdraustuju_skaicius_$(date +%Y_%m_%d).zip -c http://sodra.is.lt/Failai/Apdraustuju_skaicius.zip;
+wget -O /home/pc/Data/Projects/Atvira_sodra_duomenys/apdraustuju_pajamu_analize_$(date +%Y_%m_%d).zip -c http://atvira.sodra.lt/downloads/lt-eur/apdraustuju_pajamu_analize.zip;
+wget -O /home/pc/Data/Projects/Atvira_sodra_duomenys/finansai_$(date +%Y_%m_%d).zip -c http://atvira.sodra.lt/downloads/lt-eur/finansai.zip;
+wget -O /home/pc/Data/Projects/Atvira_sodra_duomenys/pensijos_$(date +%Y_%m_%d).zip -c http://atvira.sodra.lt/downloads/lt-eur/pensijos.zip;
+wget -O /home/pc/Data/Projects/Atvira_sodra_duomenys/pasalpos_$(date +%Y_%m_%d).zip -c http://atvira.sodra.lt/downloads/lt-eur/pasalpos.zip;
+wget -O /home/pc/Data/Projects/Atvira_sodra_duomenys/nad_pl_$(date +%Y_%m_%d).zip -c http://atvira.sodra.lt/downloads/lt-eur/nad_pl.zip;
+wget -O /home/pc/Data/Projects/Atvira_sodra_duomenys/nedarbo_ismokos_$(date +%Y_%m_%d).zip -c http://atvira.sodra.lt/downloads/lt-eur/nedarbo_ismokos.zip;
+wget -O /home/pc/Data/Projects/Atvira_sodra_duomenys/draudejai_$(date +%Y_%m_%d).zip -c http://atvira.sodra.lt/downloads/lt-eur/draudejai.zip;
+wget -O /home/pc/Data/Projects/Atvira_sodra_duomenys/apdraustieji_ketv_$(date +%Y_%m_%d).zip -c http://atvira.sodra.lt/downloads/lt-eur/apdraustieji_ketv.zip;
+wget -O /home/pc/Data/Projects/Atvira_sodra_duomenys/apdraustieji_det_$(date +%Y_%m_%d).zip -c http://atvira.sodra.lt/downloads/lt-eur/apdraustieji_det.zip;
+wget -O /home/pc/Data/Projects/Atvira_sodra_duomenys/nedarbingumas_11_1_$(date +%Y_%m_%d).csv -c http://atvira.sodra.lt/csv/lt-eur/nedarbingumas_11_1.csv;
+wget -O /home/pc/Data/Projects/Atvira_sodra_duomenys/nedarbingumas_13_1_$(date +%Y_%m_%d).csv -c http://atvira.sodra.lt/csv/lt-eur/nedarbingumas_13_1.csv;
